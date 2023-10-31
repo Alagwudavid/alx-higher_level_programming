e91e889b8c603c93d83e7933b6ceafb5dc9228d7
David Alagwu <alagwudavid2019@gmail.com>
2023-10-31 13:13:21 -0700

    python task done

---
+++ 0x00-python-hello_world/7-edges.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+word = "Holberton"
+word_first_3, word_last_2, middle_word = word[0:3], word[7:], word[4:-4]
+print(f"First 3 letters: {word_first_3}")
+print(f"Last 2 letters: {word_last_2}")
+print(f"Middle word: {middle_word}")
---
+++ 0x00-python-hello_world/9-easter_egg.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+word = '“The Zen of Python”, by TimPeters\n'
+print(word)
