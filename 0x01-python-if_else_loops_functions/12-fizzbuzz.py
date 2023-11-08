0f1aae6ff836240f216314e66d059ffb16249099
David Alagwu <alagwudavid2019@gmail.com>
2023-11-08 01:26:21 -0800

    fizzbuzz done

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    for i in range(1, 100 + 1):
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz", end=", ", )
+        elif i % 3 == 0:
+            print("FIzz", end=", ")
+        elif i % 5 == 0:
+            print("Buzz", end=", ")
+        else:
+            print(i, end=", ")
