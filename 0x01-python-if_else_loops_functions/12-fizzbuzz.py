2d4c029b354bd4bdfe2904c43176be4074dfa7dc
David Alagwu <alagwudavid2019@gmail.com>
2023-11-08 01:53:56 -0800

    fIzz to fizz updated

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -4,7 +4,7 @@ def fizzbuzz():
         if i % 3 == 0 and i % 5 == 0:
             print("FizzBuzz", end=" ")
         elif i % 3 == 0:
-            print("FIzz", end=" ")
+            print("Fizz", end=" ")
         elif i % 5 == 0:
             print("Buzz", end=" ")
         else:
