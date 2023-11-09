ce5de08c7761f80cdf733ffa21e061985d7b452d
David Alagwu <alagwudavid2019@gmail.com>
2023-11-09 06:05:38 -0800

    comb2 update

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-num_str = str(number)
-last_digit = number[-1]
-last_digit = int(last_digit)
-if last_digit > 5:
-    print("Last digit of {} is {} and is greater than 5".format(number, last_digit))
-elif last_digit < 6 and last_digit != 0:
-    print("Last digit of {} is {} and is less than 6 and not 0".format(number, last_digit))
-else:
-    print("Last digit of {} is {} and is 0 and is 0".format(number, last_digit))
+if number > 5:
+	print("Last digit of {} is {} and is greater than 5".format(number, str(number)[-1:]))
+elif number == 0:
+	print("Last digit of {} is {} and is zero".format(number, str(number)[-1:]))
+else number < 6 and number != 0:
+        print("Last digit of {} is {} and is less than 6 and not 0".format(number, str(number)[-1:]))
--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 for i in range(0, 98 + 1):
-    print('{:02d}'.format(i), end=', ')
-print('{}'.format(99))
+    if i < 99:
+        print('{:02d}'.format(i), end=', ')
+    else:
+        print('{}'.format(99))
