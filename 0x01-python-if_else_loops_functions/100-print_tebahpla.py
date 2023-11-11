89d896eb46ed7c36d1df1840431dd5d079476560
David Alagwu <alagwudavid2019@gmail.com>
2023-11-11 02:07:42 -0800

    print a-z backwards

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for i in range(ord("z"), ord("a") - 1, -1):
-    if i % 2 == 0:
+    if i % 2 != 0:
         print("{}".format(chr(i).upper()), end="")
     else:
         print("{}".format(chr(i)), end="")
