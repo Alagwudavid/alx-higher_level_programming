f23ded135587afe22a49ea02221509636230233d
Alagwudavid <alagwudavid2019@gmail.com>
2023-11-16 20:12:03 +0000

    add 0

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
+from add_0.py import add
+
 if __name__ == "__main__":
-a = 1
-b = 2
-print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+    a = 1
+    b = 2
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
