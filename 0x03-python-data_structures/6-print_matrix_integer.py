78af5b8edfe2618d6b997ab48a228d2dcace5557
David Alagwu <alagwudavid2019@gmail.com>
2023-11-22 13:06:45 -0800

    0x03 update

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    if matrix:
+        for elems in matrix:
+            i = 1
+            length = len(elems)
+
+            for elem in elems:
+                if i == length:
+                    print('{:d}'.format(elem), end='')
+                else:
+                    print('{:d}'.format(elem), end=' ')
+                i += 1
+
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    if matrix:
+        for elems in matrix:
+            i = 1
+            length = len(elems)
+
+            for elem in elems:
+                if i == length:
+                    print('{:d}'.format(elem), end='')
+                else:
+                    print('{:d}'.format(elem), end=' ')
+                i += 1
+
+            print()
