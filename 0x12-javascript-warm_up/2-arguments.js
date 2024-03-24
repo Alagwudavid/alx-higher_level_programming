c50c9b015f33e16004455860fd448b956e65db43
David Alagwu <alagwudavid2019@gmail.com>
2024-03-24 16:12:17 -0700

    changed console message

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,7 +2,7 @@
 if (process.argv.length === 2) {
     console.log("No argument");
 } else if (process.argv.length === 3) {
-    console.log("Arguments found");
-} else {
     console.log("Argument found");
+} else {
+    console.log("Arguments found");
 }
